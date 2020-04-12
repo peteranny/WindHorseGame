6cd01e6f3d94bf89a3f06d610e53ff9c56de44e7
peteranny <peteranny2@gmail.com>
2020-04-13 01:25:38 +0800

    html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: process.env.NODE_ENV || "development",
@@ -8,4 +9,5 @@ module.exports = {
     contentBase: path.join(__dirname, "dist"),
     open: true,
   },
+  plugins: [new HtmlWebpackPlugin()],
 };
