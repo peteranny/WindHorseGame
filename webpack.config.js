4882e846822cd0e93b25288edcc8ade1b0c1cc79
peteranny <peteranny2@gmail.com>
2020-04-13 01:13:06 +0800

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+module.exports = {
+  mode: process.env.NODE_ENV || "development",
+  entry: path.join(__dirname, "src/index.js"),
+  output: { path: path.join(__dirname, "dist") },
+};
