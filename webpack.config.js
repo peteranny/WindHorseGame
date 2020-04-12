80d54b55794c8dd53b29cb81ba18b80cab3810ed
peteranny <peteranny2@gmail.com>
2020-04-13 01:22:57 +0800

    dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,8 @@ module.exports = {
   mode: process.env.NODE_ENV || "development",
   entry: path.join(__dirname, "src/index.js"),
   output: { path: path.join(__dirname, "dist") },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    open: true,
+  },
 };
