4738b5e6cd0eeaa93a41d33a5bf83bdbdc26b45c
peteranny <peteranny2@gmail.com>
2020-04-15 01:36:30 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+git push $@
+NODE_ENV=production yarn build
+git add -f dist/*
+git commit -m dist -n
+git subtree split --prefix dist -b gh-pages
+git push -f origin gh-pages:gh-pages
+git branch -D gh-pages
+git reset HEAD~
+rm -rf dist
