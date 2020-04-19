e639a01917b85f41517652642531ba0f707dad23
peteranny <peteranny2@gmail.com>
2020-04-19 18:39:39 +0800

    table

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
 import React from "react";
-import useQuery from "../hooks/useQuery";
 import useMouse from "../hooks/useMouse";
 import MouseContext from "../contexts/MouseContext";
 import Screen from "./Screen";
-import Hello from "./Hello";
+import Maze from "./Maze.js";
 
 const App = () => {
   const [mouse, handleMouseClick] = useMouse();
-  const [query, setQuery] = useQuery();
-  const n = parseInt(query.n) || 0;
   return (
     <MouseContext.Provider value={mouse}>
       <Screen onClick={handleMouseClick}>
-        <Hello n={n} setN={(n) => setQuery({ ...query, n })} />
+        <Maze />
       </Screen>
     </MouseContext.Provider>
   );
