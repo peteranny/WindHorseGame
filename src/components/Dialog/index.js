7e0bdb62d3867d45f3255a8a134ab80c01f8c37c
peteranny <peteranny2@gmail.com>
2020-10-09 11:54:30 +0800

    dialog

---
+++ src/components/Dialog/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./styles.css";
+
+const Dialog = () => (
+  <div className={styles.outer}>
+    <div className={styles.inner}>
+      Hello!
+      <br />
+      World!
+    </div>
+  </div>
+);
+
+export default Dialog;
